011f5991e97ed8846208be7ecd9d75086a45b16e
bs2k <bs2k@bs2k.me>
2022-10-12 00:04:01 +0900

    chore: add lint

--- src/index.ts
+++ src/index.ts
@@ -8,63 +8,52 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-import isEven from "is-even";
-import isOdd from "is-odd";
-import { Router } from "itty-router";
+import isEven from 'is-even'
+import isOdd from 'is-odd'
+import { Router } from 'itty-router'
 
-export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-}
+const router = Router()
 
-const router = Router();
-
-router.get("/is-even/:num", ({ params }) => {
-  if (typeof params == "undefined") {
+router.get('/is-even/:num', ({ params }) => {
+  if (typeof params === 'undefined') {
     return new Response(
       JSON.stringify({
         result: null,
-        err: true,
+        err: true
       }),
       { status: 500 }
-    );
+    )
   } else {
     return new Response(
       JSON.stringify({
         result: isEven(params.num),
-        err: false,
+        err: false
       })
-    );
+    )
   }
-});
+})
 
-router.get("/is-odd/:num", ({ params }) => {
-  if (typeof params == "undefined") {
+router.get('/is-odd/:num', ({ params }) => {
+  if (typeof params === 'undefined') {
     return new Response(
       JSON.stringify({
         result: null,
-        err: true,
+        err: true
       }),
       { status: 500 }
-    );
+    )
   } else {
     return new Response(
       JSON.stringify({
         result: isOdd(params.num),
-        err: false,
+        err: false
       })
-    );
+    )
   }
-});
+})
 
-router.all("*", () => new Response("Not Found", { status: 404 }));
+router.all('*', () => new Response('Not Found', { status: 404 }))
 
 export default {
-  fetch: router.handle,
-};
+  fetch: router.handle
+}
